d6236a19e7d908b9067da4371cf1f569c9b401d4
Keith To <keithtkto@gmail.com>
2016-01-26 12:59:14 -0800

    also finish ttt rework

--- main.js
+++ main.js
@@ -2,14 +2,30 @@ console.log("Crossroads");
 
 /* Model */
 
-var won = false;
-var currentPlayer = "x";
-var board = ["","","", //0,1,2
-            "","","",  //3,4,5
-            "","","",]; //6,7,8
+var won;
+var currentPlayer;
+var board;
 
+startGame() // call function start game here, to initiatize the game, more elegent!!!
 
 // 1. START THE GAME
+
+// inputs:
+// -current player, board, wom
+
+
+// output:
+// -board is emptied, player x, won is false
+
+function startGame() {
+  var won = false;
+  var currentPlayer = "x";
+  var board = ["","","", //0,1,2
+            "","","",  //3,4,5
+            "","","",]; //6,7,8
+          };
+
+
 // 2. MOVE (CHANGE TURN)
     // Inputs:
       // Whose turn it is
@@ -29,7 +45,7 @@ var move = function(cellIndex) {
     if (currentPlayer === "X") {
       currentPlayer = "O";
     } else {
-      currentPlayer === "X";
+      currentPlayer = "X";
     }
   };
 };
@@ -39,5 +55,22 @@ var move = function(cellIndex) {
 // 3. WIN CONDITION
 
 var gameWon = function() {
+  return( (board[0] === board[1]) && (board[0] === board[2]) && (board[0] !== "")) ||
+       ((board[3] === board[4]) && (board[5] === board[3]) && (board[3] !== "")) ||
+       ((board[6] === board[7]) && (board[8] === board[6]) && (board[6] !== "") );
+        //this statment is already true or false
+};
+
+// updating the DOM, draw init state to page
+var render = function() {
+
 
 };
+
+
+
+
+
+
+
+
