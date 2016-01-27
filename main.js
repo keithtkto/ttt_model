ff637157280dc65364013ce3dd538b31bff57545
Keith To <keithtkto@gmail.com>
2016-01-26 17:51:20 -0800

    mostly notes wk2 day2

--- main.js
+++ main.js
@@ -1,76 +1,92 @@
-console.log("Crossroads");
+console.log("main.js loaded!");
 
-/* Model */
+/* MODEL */
 
-var won;
-var currentPlayer;
-var board;
-
-startGame() // call function start game here, to initiatize the game, more elegent!!!
+var won = false;
+var currentPlayer = "X";
+var board = [
+  "", "", "", // 0, 1, 2
+  "", "", "", // 3, 4, 5
+  "", "", ""  // 6, 7, 8
+];
 
 // 1. START THE GAME
-
 // inputs:
-// -current player, board, wom
-
-
+// - current player, board, won
+//
 // output:
-// -board is emptied, player x, won is false
+// - board is emptied, player is X, won is false
+var startGame = function() {
+  won = false;
+  currentPlayer = "X";
+  board = [
+    "", "", "", // 0, 1, 2
+    "", "", "", // 3, 4, 5
+    "", "", ""  // 6, 7, 8
+  ];
+};
 
-function startGame() {
-  var won = false;
-  var currentPlayer = "x";
-  var board = ["","","", //0,1,2
-            "","","",  //3,4,5
-            "","","",]; //6,7,8
-          };
-
-
-// 2. MOVE (CHANGE TURN)
-    // Inputs:
-      // Whose turn it is
-      // What cell
-
-    // Outputs:
-      // Fill in cell with the right player
-      // Calculate if there is a winner
-      // Changes turn
-
+// 2. MOVE (CHANGES TURN)
+//    inputs:
+//    - whose turn it is,
+//    - what cell
+//
+//   outputs:
+//   - fill in cell with the right player
+//   - calculate if there is a winner
+//   - turn changes
 
 var move = function(cellIndex) {
   board[cellIndex] = currentPlayer;
-  if (gameWon() ) {
-      won = true
+  if (gameWon()) {
+    won = true;
   } else {
     if (currentPlayer === "X") {
       currentPlayer = "O";
     } else {
       currentPlayer = "X";
     }
-  };
+    // currentPlayer = (currentPlayer === "X" ? "O" : "X");
+  }
 };
 
-/* currentPlayer = (currentPlayer === "X" ? "O" : "X") */
-
-// 3. WIN CONDITION
+// 3. WIN THE GAME
+//    inputs:
+//    - board
+//
+//    outputs:
+//    - true or false
 
 var gameWon = function() {
-  return( (board[0] === board[1]) && (board[0] === board[2]) && (board[0] !== "")) ||
-       ((board[3] === board[4]) && (board[5] === board[3]) && (board[3] !== "")) ||
-       ((board[6] === board[7]) && (board[8] === board[6]) && (board[6] !== "") );
-        //this statment is already true or false
+  if (
+    ((board[0] === board[1]) && (board[0] === board[2]) && board[0] !== "") ||
+    ((board[3] === board[4]) && (board[3] === board[5]) && board[3] !== "") ||
+    ((board[6] === board[7]) && (board[6] === board[8]) && board[6] !== "") ||
+    ((board[0] === board[3]) && (board[0] === board[6]) && board[0] !== "") ||
+    ((board[1] === board[4]) && (board[1] === board[7]) && board[1] !== "") ||
+    ((board[2] === board[5]) && (board[2] === board[8]) && board[2] !== "") ||
+    ((board[0] === board[4]) && (board[0] === board[8]) && board[0] !== "") ||
+    ((board[2] === board[4]) && (board[2] === board[6]) && board[2] !== "")
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-// updating the DOM, draw init state to page
+
+/* RENDER OUR VIEW */
+
 var render = function() {
+  // Render Turn Counter Component
+  var turnEl = document.getElementById("turn");
+  turnEl.textContent = "Turn: " + currentPlayer;
 
-
+  // Render Winner Component
+  var winnerEl = document.getElementById("winner");
+  if (!won) {
+    winnerEl.textContent = "Winner: ?";
+  } else {
+    winnerEl.textContent = "Winner: " + currentPlayer;
+  }
 };
-
-
-
-
-
-
-
-
